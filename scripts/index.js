ebda8a8ff3bf3993d8c79b9d810187c40fb95842
TheLegend30 <luibichenko.oleg@gmail.com>
2023-11-19 01:03:56 +0200

    Add Gallery(Demo). Few other changes.

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,65 @@
-console.log("Hello");
+let currentPersonGallery = 0;
+const quotes = new Map();
+quotes.set(
+  "Ігор Станкевич, психотерапевт",
+  "Zenith Care - це не просто платформа, а справжнє спасіння для тих, хто шукає допомогу. Раджу всім колегам приєднатися до цього чудового сервісу. Разом ми можемо зробити більше!"
+);
+
+quotes.set(
+  "Оксана Артеменко, голова клініки 'Рівновага'",
+  "Я вражена тим, як Zenith Care об'єднує спеціалістів з різних галузей, створюючи команду експертів. Разом ми можемо надавати комплексну допомогу тим, хто цього потребує."
+);
+
+quotes.set(
+  "Ганна Горчинська, психолог",
+  "Працюючи з Zenith Care, я відчуваю себе частиною спільноти, що дійсно робить різницю. Платформа забезпечує ефективний зв'язок між тими, хто потребує допомоги, і тими, хто готовий її надати."
+);
+
+const quoteTextEl = document.querySelector(".quote-text");
+const quoteAuthorEl = document.querySelector(".quote-author");
+const imgGalleryEl = document.querySelector(".quote-img");
+const rightButtonGalleryEl = document.querySelector(".gallery-button--right");
+const leftButtonGalleryEl = document.querySelector(".gallery-button--left");
+const dotsEls = document.querySelectorAll(".dot");
+
+const changeCarousel = function () {
+  dotsEls.forEach((cur) => cur.classList.remove("dot--fill"));
+  dotsEls[currentPersonGallery].classList.add("dot--fill");
+
+  imgGalleryEl.classList.add("opaque");
+  quoteTextEl.classList.add("opaque");
+  quoteAuthorEl.classList.add("opaque");
+  setTimeout(() => {
+    quoteTextEl.textContent = [...quotes.values()][currentPersonGallery];
+    quoteAuthorEl.textContent = [...quotes.keys()][currentPersonGallery];
+    imgGalleryEl.src = `/images/gallery/person${currentPersonGallery + 1}.jpg`;
+
+    imgGalleryEl.classList.remove("opaque");
+    quoteTextEl.classList.remove("opaque");
+    quoteAuthorEl.classList.remove("opaque");
+  }, 300);
+};
+
+rightButtonGalleryEl.addEventListener("click", () => {
+  currentPersonGallery++;
+  if (currentPersonGallery >= dotsEls.length) {
+    currentPersonGallery = 0;
+  }
+
+  changeCarousel();
+});
+
+leftButtonGalleryEl.addEventListener("click", () => {
+  currentPersonGallery--;
+  if (currentPersonGallery < 0) {
+    currentPersonGallery = dotsEls.length - 1;
+  }
+  changeCarousel();
+});
+
+dotsEls.forEach((cur, key) =>
+  cur.addEventListener("click", () => {
+    currentPersonGallery = key;
+    changeCarousel();
+  })
+);
